8a35ae20e985a043f4412cd825315c652b67b44c
Lethallmadness <163110451+Lethallmadness@users.noreply.github.com>
2024-03-12 10:25:01 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Pierwsze zadanie z PHP</h1>
+    <?php
+    $Iimie  =   'Cygan';
+    $liczba  =   10;
+    $liczba1 =  4.5;
+        echo "<h3>$Iimie managed to turn off <i>computer</i></h3>";
+        echo '<h3>'.$Iimie  .' managed to '. $liczba .' turn off <i>computer</i></h3>';
+        echo $liczba1;
+        echo '<br>';
+        echo $liczba+$liczba1;
+    ?>
+    
+</body>
+</html>
